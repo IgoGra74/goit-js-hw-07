cdbe46e2a140f5e898ad5c48169f6d0e029a2aef
IgorGra1 <gis1@ukr.net>
2024-01-05 20:11:52 +0200

    task-3

--- js/task-1.js
+++ js/task-1.js
@@ -7,7 +7,17 @@ console.log(`Number of categories: ${categoryItems.length}`);
 categoryItems.forEach(categoryItem => {
   const categoryName = categoryItem.querySelector('h2').textContent;
 
+  const itemSubtitle = categoryItem.querySelector('h2');
+  itemSubtitle.classList.add('js-item-subtitle');
+
+  const itemList = categoryItem.querySelector('ul');
+  itemList.classList.add('js-item-list');
+
   const categoryElements = categoryItem.querySelectorAll('ul li');
+
+  categoryElements.forEach(listItem => {
+    listItem.classList.add('js-list-item');
+  });
 
   console.log(`Category: ${categoryName}`);
   console.log(`Number of categories: ${categoryElements.length}`);
--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,39 @@
+const loginForm = document.querySelector('.login-form');
+const emailInput = loginForm.elements.email;
+const passwordInput = loginForm.elements.password;
 
+const formLabels = document.querySelectorAll('.login-form label');
+const wrapperDiv = document.createElement('div');
+
+formLabels.forEach(label => {
+  label.classList.add('js-form-label');
+  wrapperDiv.appendChild(label);
+});
+
+loginForm.prepend(wrapperDiv);
+
+wrapperDiv.classList.add('js-labels');
+
+const formBtn = document.querySelector('button');
+formBtn.classList.add('js-form-btn');
+
+loginForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const emailValue = emailInput.value.trim();
+  const passwordValue = passwordInput.value.trim();
+
+  if (emailValue === '' || passwordValue === '') {
+    alert('All form fields must be filled in');
+    return;
+  }
+
+  const formData = {
+    email: emailValue,
+    password: passwordValue,
+  };
+
+  console.log(formData);
+
+  loginForm.reset();
+});
