a7eac79e36ac444bc591f3116ed7c0dc05357c07
IgorGra1 <gis1@ukr.net>
2025-05-18 21:00:00 +0300

    Update task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -1,13 +1,13 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
-
 function changeBackgroundColor() {
   const body = document.body;
 
   const colorSpan = document.querySelector('.color');
+
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
 
   const randomColor = getRandomHexColor();
 
